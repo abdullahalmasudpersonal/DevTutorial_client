c6625dba219f84c476f1cb4a01fcf759fcdb66a8
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-12-09 11:51:08 +0600

    update about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center'>
-            <p><small>About</small></p>
+        <div style={{height:'800px'}} className='d-flex justify-content-center align-items-center'>
+            <div  class="loader">About<span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span><span class="loader__dot">.</span></div>
         </div>
     );
 };
