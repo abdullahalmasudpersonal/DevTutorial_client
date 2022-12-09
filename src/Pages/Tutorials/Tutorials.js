95d26dd3df729681a3b4490f8575ea734690ba35
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-12-10 01:11:11 +0600

    another update

--- src/Pages/Tutorials/Tutorials.js
+++ src/Pages/Tutorials/Tutorials.js
@@ -4,7 +4,7 @@ import Tutorial from './Tutorial';
 const Tutorials = () => {
     const [tutorials, setTutorials] = useState([]);
     useEffect( () =>{
-        fetch('Tutorials.json')
+        fetch('http://localhost:5000/tutorials')
         .then(res => res.json())
         .then(data => setTutorials(data));
     }, []);
